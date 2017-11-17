04a1ad6759b4e570047ad81a29b24a770d7fd35c
Peter Ho Yeung Chan <peterchan01272002@gmail.com>
2017-11-17 08:23:09 +0000

    Starting working on Sign Up page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,22 +3,25 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get root_path
+    get help_url
     assert_response :success
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get root_path
+    get contact_url
     assert_response :success
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get root_path
+    get about_url
     assert_response :success
   end
 
