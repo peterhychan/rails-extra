c744605ce7c2e9eca0d7188518dea6d195c3c842
Peter Ho Yeung Chan <peterchan01272002@gmail.com>
2017-11-18 08:24:14 +0000

    Added Log In

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,19 +11,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
-      flash[:success] = "Welcome to Submission RAM"
+      log_in @user
+      flash[:success] = "Welcome to the Submission RAM"
       redirect_to @user
-
     else
       render 'new'
     end
   end
-  
+
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-  end
+    end
 end
