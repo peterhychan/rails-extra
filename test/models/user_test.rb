d280661390bdcad95336d940869992e8f7d39183
Peter Ho Yeung Chan <peterchan01272002@gmail.com>
2017-11-18 06:55:42 +0000

    Completed SignUP

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,10 +7,6 @@ class UserTest < ActiveSupport::TestCase
                      password: "foobar", password_confirmation: "foobar")
   end
 
-  test "should be valid" do
-    assert @user.valid?
-  end
-
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
@@ -31,14 +27,6 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email validation should accept valid addresses" do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn]
-    valid_addresses.each do |valid_address|
-      @user.email = valid_address
-      assert @user.valid?, "#{valid_address.inspect} should be valid"
-    end
-  end  
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
